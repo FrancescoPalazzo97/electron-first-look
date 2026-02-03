8c95edebbef73209722a720099dff742ae3c2bd3
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-02-03 17:17:47 +0100

    create basic app

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile("index.html");
